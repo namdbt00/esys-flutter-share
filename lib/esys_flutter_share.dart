57bf1cd2e32b881fab2695dbccd64f54f308c2fd
Do Bui Thanh Nam <donam2539@gmail.com>
2021-09-24 16:25:24 +0700

    Update esys_flutter_share.dart

--- lib/esys_flutter_share.dart
+++ lib/esys_flutter_share.dart
@@ -5,23 +5,23 @@ import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 
 class Share {
-  static const MethodChannel _channel = const MethodChannel(
+  static const MethodChannel? _channel = const MethodChannel(
       'channel:github.com/orgs/esysberlin/esys-flutter-share');
 
   /// Sends a text to other apps.
-  static void text(String title, String text, String mimeType) {
-    Map argsMap = <String, String>{
+  static void text(String? title, String? text, String? mimeType) {
+    Map? argsMap = <String, String>{
       'title': '$title',
       'text': '$text',
       'mimeType': '$mimeType'
     };
-    _channel.invokeMethod('text', argsMap);
+    _channel!.invokeMethod('text', argsMap);
   }
 
   /// Sends a file to other apps.
-  static Future<void> file(
-      String title, String name, List<int> bytes, String mimeType, {String text = ''}) async {
-    Map argsMap = <String, String>{
+  static Future<void>? file(
+      String? title, String? name, List<int?>? bytes, String? mimeType, {String? text = ''}) async {
+    Map? argsMap = <String?, String?>{
       'title': '$title',
       'name': '$name',
       'mimeType': '$mimeType',
@@ -30,17 +30,17 @@ class Share {
 
     final tempDir = await getTemporaryDirectory();
     final file = await new File('${tempDir.path}/$name').create();
-    await file.writeAsBytes(bytes);
+    await file.writeAsBytes(bytes as List<int>);
 
-    _channel.invokeMethod('file', argsMap);
+    _channel!.invokeMethod('file', argsMap);
   }
 
   /// Sends multiple files to other apps.
-  static Future<void> files(
-      String title, Map<String, List<int>> files, String mimeType, {String text = ''}) async {
-    Map argsMap = <String, dynamic>{
+  static Future<void>? files(
+      String? title, Map<String?, List<int?>?>? files, String? mimeType, {String? text = ''}) async {
+    Map? argsMap = <String?, dynamic>{
       'title': '$title',
-      'names': files.entries.toList().map((x) => x.key).toList(),
+      'names': files!.entries.toList().map((x) => x.key).toList(),
       'mimeType': mimeType,
       'text': '$text'
     };
@@ -49,9 +49,9 @@ class Share {
 
     for (var entry in files.entries) {
       final file = await new File('${tempDir.path}/${entry.key}').create();
-      await file.writeAsBytes(entry.value);
+      await file.writeAsBytes(entry.value as List<int>);
     }
 
-    _channel.invokeMethod('files', argsMap);
+    _channel!.invokeMethod('files', argsMap);
   }
 }
